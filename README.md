5c12dffed56601259e8db2f86ca784cf53f6fb32
Mark Soper <masoper@gmail.com>
2011-08-24 11:44:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+node-runkeeper - Node.js Client for Runkeeper Health Graph API
+==============================================================
+
+#### Author: [Mark Soper](masoper@gmail.com)
+
+A client/wrapper for the Runkeeper Health Graph API:
+
+http://developer.runkeeper.com/healthgraph
+
---
+++ test/test-getToken.js
@@ -0,0 +1,19 @@
+
+/*
+ * test-getToken.js:  Test 2nd step of Runkeeper Health Graph API handshake
+ *
+ * usage:  node test-getToken.js --code <authorization_code>
+ * 
+ * <authorization_code> is returned by the api as described in step 1 here:
+ * http://developer.runkeeper.com/healthgraph/registration-authorization
+ *
+ */
+
+var argv = require('optimist').argv;
+
+console.log("Getting token for code: " + argv.code);
+
+var runkeeper = require('../lib/runkeeper');
+
+var rk = new runkeeper.HealthGraphClient();
+
